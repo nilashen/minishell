52b1b391923a24e8e20efc7c558dcd9f7e76b62d
Nilufar Ashena <nashena@2-I-2.42heilbronn.de>
2025-08-03 18:40:48 +0200

    - Added  to check if a command is a supported built-in (echo, cd, pwd, export, unset, env, exit). - Added  to handle execution of built-in commands. - Started implementation of  to handle one-command pipelines. - Added  stub with support for executing built-ins.

---
+++ execution.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nashena <nashena@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 16:14:43 by nashena           #+#    #+#             */
+/*   Updated: 2025/08/03 17:10:43 by nashena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_mysh(char *cmd)
+{
+	if (!cmd)
+		return (0);
+	if (ft_strncmp(cmd, "echo", 5) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "cd", 3) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "pwd", 4) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "export", 7) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "unset", 6) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "env", 4) == 0)
+		return (1);
+	if (ft_strncmp(cmd, "exit", 5) == 0)
+		return (1);
+	return (0);
+}
+
+int	execute_mysh(t_shell *shell, t_cmd *cmd)
+{
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	if (ft_strncmp(cmd->args[0], "echo", 5) == 0)
+		return (mysh_echo(cmd->args));
+	if (ft_strncmp(cmd->args[0], "cd", 3) == 0)
+		return (mysh_cd(cmd->args));
+	if (ft_strncmp(cmd->args[0], "pwd", 4) == 0)
+		return (mysh_pwd());
+	if (ft_strncmp(cmd->args[0], "export", 7) == 0)
+		return (mysh_export(cmd->args, &shell->envp));
+	if (ft_strncmp(cmd->args[0], "unset", 6) == 0)
+		return (mysh_unset(cmd->args, &shell->envp));
+	if (ft_strncmp(cmd->args[0], "env", 4) == 0)
+		return (mysh_env(shell->envp));
+	if (ft_strncmp(cmd->args[0], "exit", 5) == 0)
+		return (mysh_exit(cmd->args));
+	return (1);
+}
+int	commands_execution(t_shell *shell)
+{
+	t_cmd	*current;
+	int		cmd_count;
+
+	if (!shell || !shell->cmds)
+		return (1);
+	current = shell->cmds;
+	cmd_count = 0;
+	while (current)
+	{
+		cmd_count++;
+		current = current->next;
+	}
+	if (cmd_count == 1)
+		return (execute_single_cmd(shell, shell->cmds));
+}
+int	execute_single_cmd(t_shell *shell, t_cmd *cmd)
+{
+	pid_t	pid;
+	int		status;
+	char	*path;
+
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (1);
+	if (is_mysh(cmd->args[0]))
+		return (execute_mysh(shell, cmd));
+}
--- minishell.h
+++ minishell.h
@@ -6,20 +6,37 @@
 /*   By: nashena <nashena@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 11:02:09 by nashena           #+#    #+#             */
-/*   Updated: 2025/08/03 13:34:55 by nashena          ###   ########.fr       */
+/*   Updated: 2025/08/03 17:13:11 by nashena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-
+/* PROJECT INCLUDES */
 # include "library/libutils.h"
+/* STANDARD LIBRARIES */
 # include <errno.h>
 # include <stdio.h>
 # include <signal.h>
 # include <string.h>
 # include <sys/stat.h>
 # include <sys/wait.h>
+
+/* DATA STRUCTURES */
+typedef struct s_cmd
+{
+	char				**args;
+	struct s_cmd		*next;
+}	t_cmd;
+
+typedef struct s_shell
+{
+	char				**envp;
+	int					last_exit_status;
+	t_cmd				*cmds;
+}	t_shell;
+
+/* FUNCTION PROTOTYPES */
 int		mysh_echo(char **argv);
 int		mysh_pwd(void);
 int		mysh_cd(char **argv);
@@ -27,7 +44,12 @@ int		mysh_env(char **envp);
 int		mysh_exit(char **argv);
 int		main(int argc, char **argv, char **envp);
 int		mysh_export(char **args, char ***envp);
+int		env_count(char **envp);
 int		env_set(char ***envp, char *key, char *value);
 int		mysh_unset(char **argv, char ***envp);
 int		env_unset(char ***envp, char *key);
+int		commands_execution(t_shell *shell);
+int		execute_single_cmd(t_shell *shell, t_cmd *cmd);
+int		is_mysh(char *cmd);
+int		execute_mysh(t_shell *shell, t_cmd *cmd);
 #endif
