f188062fa186e42e9779750950ee784a2c052b3b
NJK <nabin.jk7512@gmail.com>
2025-08-04 11:33:11 +0200

    Updated with new struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,8 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*                               INCLUDES                                     */
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
@@ -13,20 +15,27 @@
 # include <fcntl.h>
 # include <errno.h>
 
-/* ************************************************************************** */
+// External environ variable
+extern char **environ;
+
 /*                               DEFINES                                      */
-/* ************************************************************************** */
 
 # define PROMPT "minishell$ "
 # define MAX_PATH 1024
 
-/* ************************************************************************** */
 /*                               STRUCTURES                                   */
-/* ************************************************************************** */
+
+// Simple command structure
+typedef struct s_cmd
+{
+	char	**args;		// Array of arguments (args[0] is the command)
+	int		argc;		// Number of arguments
+}	t_cmd;
 
 // Global variable for signal handling (only one allowed)
 extern int g_signal;
 
+/*                               FUNCTION PROTOTYPES                         */
 
 // Main functions
 int		main(int argc, char **argv, char **envp);
@@ -41,5 +50,15 @@ void	setup_signals(void);
 // Utility functions
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+
+// Parsing functions
+t_cmd	*parse_command(char *input);
+void	free_command(t_cmd *cmd);
+char	**split_input(char *input);
+int		count_words(char *input);
+
+// Execution functions
+int		execute_command(t_cmd *cmd);
+char	*find_command_path(char *cmd);
 
 #endif
