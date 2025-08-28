9d3de6aa211a77beaa724ace11b06a88c243600a
nilashen <nilufar.ashena@gmail.com>
2025-08-28 16:48:09 +0200

    Update ft_putnbr_fd.c
     ft_putnbr_fd is addded.

---
+++ includes/libft/ft_putnbr_fd.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write (fd, "-", 1);
+		write (fd, "2147483648", 10);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -1 * n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd (n / 10, fd);
+	}
+	c = (n % 10) + 48;
+	write (fd, &c, 1);
+}
