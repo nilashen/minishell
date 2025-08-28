6ce3121622304644f23af9ca29397d64aa50e92b
nilashen <nilufar.ashena@gmail.com>
2025-08-28 16:55:34 +0200

    Update ft_memcmp.c
    ft_memcmp is added.

---
+++ includes/libft/ft_memcmp.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*temp_s1;
+	const unsigned char	*temp_s2;
+	size_t				index;
+
+	temp_s1 = (const unsigned char *)s1;
+	temp_s2 = (const unsigned char *)s2;
+	index = 0;
+	while (index < n)
+	{
+		if (*(temp_s2 + index) != *(temp_s1 + index))
+			return (*(temp_s1 + index) - *(temp_s2 + index));
+		index++;
+	}
+	return (0);
+}
