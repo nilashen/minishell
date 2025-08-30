f905d30268a74d25ab01e78f8e67231bf6c15b3b
NJK <nabin.jk7512@gmail.com>
2025-08-30 23:04:20 +0200

    added the exit logic for invalid arguments

--- builtin/exit.c
+++ builtin/exit.c
@@ -1,29 +1,83 @@
 #include "../includes/minishell.h"
 
-void	ft_built_exit_cmd(t_state *state, t_cluster *cluster)
+static int	ft_is_valid_number(char *str)
 {
 	int	i;
 
-	i = -1;
+	if (!str || !str[0])
+		return (0);
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static long	ft_safe_atol(char *str, int *overflow)
+{
+	long	result;
+	int		sign;
+	int		i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		if (result > (LONG_MAX - (str[i] - '0')) / 10)
+		{
+			*overflow = 1;
+			return (0);
+		}
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+static void	ft_exit_with_error(t_state *state, char *arg, char *msg, int code)
+{
+	ft_putstr_fd("exit\nminishell: exit: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(msg, 2);
+	ft_full_free(state, code);
+}
+
+void	ft_built_exit_cmd(t_state *state, t_cluster *cluster)
+{
+	long	exit_code;
+	int		overflow;
+
 	if (cluster->cmd[1] != NULL)
 	{
-		while (cluster->cmd[1][++i])
-		{
-			if (ft_isdigit(cluster->cmd[1][i]) == 0
-			&& (cluster->cmd[1][i] != '-' || i != 0))
-			{
-				ft_putstr_fd("exit\nminishell: exit: ", 2);
-				ft_putstr_fd(cluster->cmd[1], 2);
-				ft_putstr_fd(": numeric argument required\n", 2);
-				ft_full_free(state, 255);
-			}
-		}
+		if (!ft_is_valid_number(cluster->cmd[1]))
+			ft_exit_with_error(state, cluster->cmd[1], 
+				": numeric argument required\n", 255);
+		overflow = 0;
+		exit_code = ft_safe_atol(cluster->cmd[1], &overflow);
+		if (overflow)
+			ft_exit_with_error(state, cluster->cmd[1], 
+				": numeric argument required\n", 255);
 		if (cluster->cmd[2] != NULL)
 		{
 			ft_putstr_fd("exit\nminishell: exit: too many arguments\n", 2);
-			ft_full_free(state, 1);
+			state->error = 1;
+			return;
 		}
-		ft_full_free(state, ft_atoi(cluster->cmd[1]));
+		ft_full_free(state, (int)(exit_code % 256));
 	}
 	ft_full_free(state, 0);
 }
