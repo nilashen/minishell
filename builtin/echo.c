f6fe6bd02eb9bae4ab3c345fdc718da8efa1af80
NJK <nabin.jk7512@gmail.com>
2025-08-30 23:03:30 +0200

    added the flow for the handling empty args

--- builtin/echo.c
+++ builtin/echo.c
@@ -23,30 +23,50 @@ static int	ft_flag_check(char **cmd)
 	return (i);
 }
 
+static int	ft_should_print_newline(int index)
+{
+	if (index == 1)
+		return (1);
+	return (0);
+}
+
+static void	ft_print_args(char **cmd, int fd, int start_index)
+{
+	int	i;
+
+	i = start_index;
+	while (cmd[i])
+	{
+		write(fd, cmd[i], ft_strlen(cmd[i]));
+		if (cmd[i + 1] != NULL)
+			write(fd, " ", 1);
+		i++;
+	}
+}
+
+static void	ft_handle_empty_args(int fd, int print_newline, t_state *state)
+{
+	if (print_newline)
+		write(fd, "\n", 1);
+	state->error = 0;
+}
+
 void	ft_echo(t_cluster *cluster, t_state *state)
 {
-	char	**cmd;
-	int		i;
-	int		fd;
-	int		index;
+	int	fd;
+	int	index;
+	int	print_newline;
 
 	fd = cluster->files->fd_output;
 	index = ft_flag_check(cluster->cmd);
-	i = index;
-	cmd = cluster->cmd;
-	if (cmd[1] == NULL)
+	print_newline = ft_should_print_newline(index);
+	if (cluster->cmd[1] == NULL)
 	{
-		write(fd, "\n", 2);
+		ft_handle_empty_args(fd, print_newline, state);
 		return ;
 	}
-	while (cmd[index])
-	{
-		write(fd, cmd[index], ft_strlen(cmd[index]));
-		if (cmd[index + 1] != NULL)
-			write(fd, " ", 1);
-		index++;
-	}
-	if (i == 1)
+	ft_print_args(cluster->cmd, fd, index);
+	if (print_newline)
 		write(fd, "\n", 1);
 	state->error = 0;
 }
